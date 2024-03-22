a18acec7b4a73ce70901ad9c1a05a22204e700b6
Ritvik-05 <agrawalritvik05@gmail.com>
2024-03-22 17:00:56 +0530

    added vercel.json file

--- backend/app.js
+++ backend/app.js
@@ -91,6 +91,20 @@ app.use(function(err, req, res, next) {
   });
 });
 
+// Your code
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+          if(err) {
+              res.status(500).send(err)
+          }
+      });
+  })
+}
+// Your code
+
 
 module.exports = app;
 
